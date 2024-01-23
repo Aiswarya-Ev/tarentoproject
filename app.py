cca9fda6b45466dce6259460bd9fb6e438accf76
Aiswarya-Ev <aiswaryaev63@gmail.com>
2024-01-23 15:57:33 +0530

    ok

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("hello aisu")
+#print("hello aisu")
+import mysql.connector
+
+# Establishing a connection to MySQL
+connection = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="password@123"
+)
+cursor = connection.cursor()
+cursor.execute("CREATE DATABASE abc")
